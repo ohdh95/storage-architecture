30662f9c626ca4f69788ea691427a28a12a17853
ohdh95 <ohdh95@gmail.com>
2025-09-26 17:24:01 +0900

    test.sh 추가

---
+++ test.sh
@@ -0,0 +1,10 @@
+make
+./ftl_test input.txt a.txt
+./ftl_test ./lab2_tc/input1.txt 1.txt
+./ftl_test ./lab2_tc/input2.txt 2.txt
+./ftl_test ./lab2_tc/input3.txt 3.txt
+./ftl_test ./lab2_tc/input4.txt 4.txt
+./ftl_test ./lab2_tc/input5.txt 5.txt
+./ftl_test ./lab2_tc/input6.txt 6.txt
+./ftl_test ./lab2_tc/input7.txt 7.txt
+./ftl_test ./lab2_tc/input8.txt 8.txt
