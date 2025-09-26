fd87db47c8b10ab370fe7c4c44146cbaba0bb979
ohdh95 <ohdh95@gmail.com>
2025-09-26 17:31:21 +0900

    sync

--- ftl.c
+++ ftl.c
@@ -1,5 +1,5 @@
 /*
- * Lab #2 : Page Mapping FTL Simulator
+ * Lab #2 : Page usedping FTL Simulator
  *  - Storage Architecture, SSE3069
  *
  * TA: Jinwoo Jeong, Hyunbin Kang
@@ -12,7 +12,7 @@
 #include "ftl.h"
 
 u32 pmt[N_LPNS];
-unsigned char map[N_BANKS][N_USER_BLOCKS_PB];
+unsigned char used[N_BANKS][N_USER_BLOCKS_PB];
 int freeblock;
 
 static void garbage_collection(u32 bank)
@@ -40,7 +40,7 @@ void ftl_open()
         pmt[i] = -1;
     }
 
-    // GC trigger에서 freeblock이 user free block의 개수가 1 ? or 총 free blcok의 개수가 1 ?
+    // GC trigger에서 freeblock이 user free block의 개수가 1 ? or 총 free blcok의 개수가 1 ? 아마도 user free block의 개수인듯
     freeblock = N_BANKS * N_USER_BLOCKS_PB;
 }
 
@@ -84,17 +84,22 @@ that you issue in this function
         int page = -1;
 
         for (int i = 0; i < N_USER_BLOCKS_PB; i++) {
-            if (map[bank][i] == 0) {
+            if (used[bank][i] == 0) {
                 block = i / PAGES_PER_BLK;
                 page = i % PAGES_PER_BLK;
-                map[bank][i] = 1;
+                used[bank][i] = 1;
                 break;
             }
         }
 
+        if (lba % SECTORS_PER_PAGE != 0) {
+            printf("이런 경우가 있음??\n");
+        }
         for (int i = 0; i < nsect; i++) {
         
         }
+
+        pmt[lpn] = bank * N_USER_BLOCKS_PB * PAGES_PER_BLK + block * PAGES_PER_BLK + page;
     }
 
     
